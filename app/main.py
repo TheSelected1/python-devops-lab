b7355846b4527f0b7ee74c3df970656392b334f1
Nathan <kiren.nguyen@gmail.com>
2026-04-07 13:25:07 +0700

    add in version for endpoint

--- app/main.py
+++ app/main.py
@@ -14,5 +14,9 @@ def home():
 def healthz():
     return jsonify({"status": "ok"})
 
+@app.route("/version")
+def version():
+    return jsonify({"version": "1.0.0"})
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
