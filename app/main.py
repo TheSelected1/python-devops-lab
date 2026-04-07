5327a7ae4de7302a0178cbe1202245282590edcf
Nathan <kiren.nguyen@gmail.com>
2026-04-07 13:56:33 +0700

    Update final version for Python app on test branch as well

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ def healthz():
 
 @app.route("/version")
 def version():
-    return jsonify({"version": "1.0.1-test"})
+    return jsonify({"version": "1.0.1-Final"})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
